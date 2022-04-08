48ff76d0cec515b2fe1f08a83e632e2336471a13
nova <nova.corps1010@outlook.com>
2022-04-08 13:46:22 -0400

    basic project

--- part01/index.js
+++ part01/index.js
@@ -70,6 +70,7 @@ console.log(itemsList.firstChild);
 console.log(itemsList.lastChild);
 */
 
+/*
 // Create elements
 let newDiv = document.createElement('div');
 
@@ -78,7 +79,6 @@ newDiv.className = 'hello';
 
 // Add id
 newDiv.id = 'hello1';
-
 // Add attr
 newDiv.setAttribute('title', 'hello Div');
 
@@ -96,4 +96,88 @@ console.log(newDiv);
 newDiv.style.fontSize = '30px';
 // Add the newDiv to the HTML on the page
 container.insertBefore(newDiv, h1);
+*/
+
+// Part 3: Playing with function inside elements
+
+// random things in part 3, so boring
+
+
+// Final project
+
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+const filter = document.getElementById('filter');
+
+// form submit event
+form.addEventListener('submit', addItem);
+
+// remove from list event
+itemList.addEventListener('click', removeItem);
+
+// Filter event
+filter.addEventListener('keyup', filterItems)
+
+
+// FUNCTIONS
+
+function filterItems(e) {
+    // convert text to lower case
+    let text = e.target.value.toLowerCase();
+    
+    // get list
+    let items = itemList.getElementsByTagName('li');
+
+    // convert to an array
+    Array.from(items).forEach(function(item) {
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        }
+        else {
+            item.style.display = 'none';
+        }
+    })
+}
+
+
+function addItem(e) {
+    // avoid normal submission of the form
+    e.preventDefault();
+    console.log(1);
+
+    // get input value
+    let newItem = document.getElementById('item').value;
+    // create a new li element
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    
+    // add text input node with input value
+    li.appendChild(document.createTextNode(newItem));
+    
+    // create delete button element
+    let deleteButton = document.createElement('button');
+    
+    // add classes to del button
+    deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+
+    // appendt text node
+    deleteButton.appendChild(document.createTextNode(' X'));
+
+    // append button to li
+    li.appendChild(deleteButton);
+
+    // append li to list
+    itemList.appendChild(li);
+}
+
+
+function removeItem(e) {
+    if(e.target.classList.contains('delete')) {
+        if(confirm('Are you sure')) {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
 
